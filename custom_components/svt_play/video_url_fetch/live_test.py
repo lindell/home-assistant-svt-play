44d66066e37dfffd24370d54e6a1071139c2c37d
Johan Lindell <johan@lindell.me>
2025-12-01 20:40:44 +0100

    Formatting

--- custom_components/svt_play/video_url_fetch/live_test.py
+++ custom_components/svt_play/video_url_fetch/live_test.py
@@ -1,37 +1,43 @@
-from video_fetch import video_url_by_channel, video_id_by_time, video_url_by_video_id, suggested_video_id, random_video_id
+from video_fetch import (
+    video_url_by_channel,
+    video_id_by_time,
+    video_url_by_video_id,
+    suggested_video_id,
+    random_video_id,
+)
 import pytest
 
 
 def test_rapport_by_time():
-    id = video_id_by_time('rapport')
+    id = video_id_by_time("rapport")
     url = video_url_by_video_id(id)
-    assert url.startswith('http')
+    assert url.startswith("http")
 
 
 def test_rapport_by_suggested():
-    id = suggested_video_id('rapport')
+    id = suggested_video_id("rapport")
     url = video_url_by_video_id(id)
-    assert url.startswith('http')
+    assert url.startswith("http")
 
 
 def test_channels():
     errors = []
 
-    for channel in ['svt1', 'svt2', 'barnkanalen', 'kunskapskanalen', 'svt24']:
+    for channel in ["svt1", "svt2", "barnkanalen", "kunskapskanalen", "svt24"]:
         url = video_url_by_channel(channel)
-        if not url.startswith('http'):
+        if not url.startswith("http"):
             errors.append(channel)
     assert not errors
 
 
 def test_not_found_by_time():
     with pytest.raises(Exception, match="Could not find program with id: bogus"):
-        video_id_by_time('bogus')
+        video_id_by_time("bogus")
 
 
 def test_not_found_by_suggested():
     with pytest.raises(Exception, match="Could not find program with id: bogus"):
-        suggested_video_id('bogus')
+        suggested_video_id("bogus")
 
 
 def test_not_found_channel():
@@ -40,10 +46,10 @@ def test_not_found_channel():
 
 
 def test_random():
-    id = random_video_id('aktuellt')
+    id = random_video_id("aktuellt")
     same_counter = 0
     for _ in range(5):
-        new_id = random_video_id('aktuellt')
+        new_id = random_video_id("aktuellt")
         print(new_id)
         if id == new_id:
             same_counter += 1
